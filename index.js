85eda0e9ddd80934f2f953c5c29cb3ee6e03026b
Adrian Stainforth <adrian.stainforth@gmail.com>
2016-10-19 22:48:11 +0100

    fixing index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+
+exports.mock_class = require('./lib/mock_class');
+exports.spies = require('./lib/spy_maanger');
+exports.stubs = require('./lib/stub_inner');
+exports.helper = require('./lib/stubs_spies_helper');
