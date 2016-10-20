7f948992f0fdfff80e8c65220abb27d39ed26b14
Adrian Stainforth <adrian.stainforth@gmail.com>
2016-10-20 14:15:37 +0100

    fix errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 exports.default = require('./lib/index');
 exports.mock_class = require('./lib/mock_class');
-exports.spies = require('./lib/spy_maanger');
+exports.spies = require('./lib/spy_manager');
 exports.stubs = require('./lib/stub_inner');
 exports.helper = require('./lib/stubs_spies_helper');
