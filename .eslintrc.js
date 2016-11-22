fdcce111ef0e9f8a77f309c831ec2a780f73cdcf
Adrian Stainforth <adrian.stainforth@gmail.com>
2016-11-22 10:56:11 +0000

    add yarn

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": "morsedigital",
+  "globals": {
+    "fdescribe": false,
+    "describe": false,
+    "beforeEach": false,
+    "afterEach": false,
+    "it": false,
+    "expect": false,
+    "spyOn": false,
+    "I18n": false,
+    "jasmine": false
+  },
+  "rules": {
+    "require-jsdoc": 0
+  }
+};
