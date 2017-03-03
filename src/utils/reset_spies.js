19831cda7b30a698880a1a244aacfb58f638bc7f
Adrian Stainforth <adrian.stainforth@gmail.com>
2017-03-03 09:00:00 +0000

    fixing reset

--- src/utils/reset_spies.js
+++ src/utils/reset_spies.js
@@ -2,16 +2,24 @@ import _ from 'lodash';
 
 import {
   checkObj
-  , getKeys
+  // , getKeys
 } from './create_spies';
 
 const checkSpy = (im)=>im.has('spy') &&
         _.isPlainObject(im.get('spy'));
 
+// const getKeys = (item)=>{
+//   let keys = item.get('keys');
+//   return keys.map((key)=>{
+//     if (Map.isMap(key)) return key.get('title');
+//     return key;
+//   }).toArray();
+// };
+
 export default (item)=>{
   if (checkObj(item) && checkSpy(item)){
     let spy = item.get('spy');
-    let keys = getKeys(item);
+    let keys = Object.keys(spy);
     keys.forEach((key)=>{
       spy[key].mockReset();
     });
